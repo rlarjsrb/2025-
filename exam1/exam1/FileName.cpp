ac791ad082953aad48e4c74b60f1345edbcf1931
rlarjsrb <rjsrbrla0926@gmail.com>
2025-10-27 17:59:09 +0900

    exam1
    1

---
+++ exam1/exam1/FileName.cpp
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int rec_func(int n)
+{
+	if (n == 1)
+				return 1;
+		else
+				return n + rec_func(n - 1);
+}
+
+int main(void)
+{
+	int result;
+	result = rec_func(10);
+	printf("%d\n", result);
+		return 0;
+}
